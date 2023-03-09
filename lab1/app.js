39dfffcdce58c3985e628746a134164764505cb0
thotvte <thotvpd07294@fpt.edu.vn>
2023-03-09 17:19:17 +0700

    nop bai tap lab1

---
+++ lab1/app.js
@@ -0,0 +1,15 @@
+//file js tinh tong hai so a,b nhap tu ban phim
+//nhap gia tri cho a la mot so nguyen tu ban phim
+var a = prompt("nhap vao so nguyen a: ");
+//ep kieu gia tri a tu string sang so nguyen
+var a1 = parseInt(a);
+//nhap gia tri cho b la mot so nguyen tu ban phim
+var b = prompt("nhap vao so nguyen b: ");
+//ep kieu gia tri b tu string sang so nguyen
+var b1 = parseInt(b);
+//tinh tong hai so a,b
+var c = a1+b1;
+//in thong tin ra man hinh su dung document
+document.writeln("Gia tri cua c = "+(c));
+//in thong tin ra man hinh su dung console.log
+console.log("Gia tri cua c = "+(c));
